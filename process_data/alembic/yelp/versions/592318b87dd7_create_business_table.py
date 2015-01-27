59de20b2c7dee914f4489db7fa7b171c96447713
Rui Jiang <rui.jiang.dev@gmail.com>
2015-01-26 18:50:56 -0800

    update alembic.ini with database configuration

---
+++ process_data/alembic/yelp/versions/592318b87dd7_create_business_table.py
@@ -0,0 +1,24 @@
+"""create business table
+
+Revision ID: 592318b87dd7
+Revises: 
+Create Date: 2015-01-26 17:34:00.756720
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '592318b87dd7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
