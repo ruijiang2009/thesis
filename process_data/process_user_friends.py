d62ef99f5d56ca16e5acc5a989483825a4cd0121
Rui Jiang <rui.jiang.dev@gmail.com>
2015-01-28 17:23:59 -0800

    add process data for user and friendship data

---
+++ process_data/process_user_friends.py
@@ -0,0 +1,30 @@
+import os
+import simplejson as json
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from model import User, Friendship
+
+engine = create_engine('postgresql://ruijiang:@localhost/yelp', echo=False)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+dir = 'data'
+file = 'yelp_academic_dataset_user.json'
+fp = open( os.path.join(dir, file) )
+
+for line in fp:
+    if line is not None or line != '':
+        try:
+            user_line = json.loads(line)
+            user1 = session.query(User).filter(User.user_id==user_line['user_id']).first()
+            for user2 in user_line['friends']:
+                db_user2 = session.query(User).filter(User.user_id==user2)
+                if db_user2.count() != 0:
+                    friendship = Friendship(user1=user1.user_id, user2=user2)
+                    session.add(friendship)
+            session.commit()
+        except UnicodeEncodeError:
+            print "UnicodeEncodeError"
