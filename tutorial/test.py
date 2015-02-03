b3b607d422c661300e637d239eb663e1b5e9a7d1
Rui Jiang <rui.jiang.dev@gmail.com>
2015-02-03 11:57:26 -0800

    add tutorial related files

---
+++ tutorial/test.py
@@ -0,0 +1,33 @@
+from gensim import corpora, models, similarities
+import os
+
+documents = ["Human machine interface for lab abc computer applications",
+             "A survey of user opinion of computer system response time",
+             "The EPS user interface management system",
+             "System and human system engineering testing of EPS",
+             "Relation of user perceived response time to error measurement",
+             "The generation of random binary unordered trees",
+             "The intersection graph of paths in trees",
+             "Graph minors IV Widths of trees and well quasi ordering",
+             "Graph minors A survey"]
+
+# remove common words and tokenize
+stoplist = set('for a of the and to in'.split())
+texts = [[word for word in document.lower().split() if word not in stoplist]
+         for document in documents]
+# remove words that appear only once
+all_tokens = sum(texts, [])
+tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+texts = [[word for word in text if word not in tokens_once]
+         for text in texts]
+
+print(texts)
+
+dir = 'output_data'
+file = 'test_token.txt'
+fp = open(os.path.join(dir, file) , 'wb')
+
+for text in texts:
+    fp.write(str(text) + '\n')
+
+fp.close()
